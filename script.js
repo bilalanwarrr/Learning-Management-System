ba75e08eb686359abbac766da4002f290e33559c
bilalanwarrr <bilalanwer2001@gmail.com>
2021-12-25 16:56:33 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+$(document).ready(function(){
+
+
+    $("#menu").click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $("#login").click(function(){
+        $('.login-form').addClass('popup');
+    });
+
+    $('.login-form form .fa-times').click(function(){
+        $('.login-form').removeClass('popup');
+    });
+
+    $("#signup").click(function(){
+        $('.signup-form').addClass('popup');
+    });
+
+    $('.signup-form form .fa-times').click(function(){
+        $('.signup-form').removeClass('popup');
+    });
+
+    $(window).on('load scroll',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+});
+
